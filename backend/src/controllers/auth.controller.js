2d0d4acf8a26585bc2932453b7590b9f7fb888de
synergisticgreed <sameergptnsr@gmail.com>
2025-02-06 23:13:53 +0530

    Added user signup functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,48 @@
-export const signup=(req,res)=>{
-    res.send("signup route")
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+export const signup= async (req,res)=>{
+    const {fullName, email, password} = req.body
+    try {
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+        
+        //hash password
+        if(password.length<6){
+            return res.status(400).json({message: "Password must be at least 6 characters"});
+        }
+        const user = await User.findOne({email})
+        if(user) return res.status(400).json({message: "Email already exists"});
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser=new User({
+            fullName: fullName,    //we can also write only fullName
+            email: email,          ////we can also write only email
+            password: hashedPassword
+        })
+
+
+        if(newUser){
+            //generate jwt token here
+            generateToken(newUser._id,res)
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        }else{
+            res.status(400).json({message: "Invalid user data"});
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message: "Internal Server Error"})
+        
+    }
 };
 
 export const login=(req,res)=>{
