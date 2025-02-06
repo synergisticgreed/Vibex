79fd538f038ac149266ccfef32fb92764ce78837
synergisticgreed <sameergptnsr@gmail.com>
2025-02-07 01:17:56 +0530

    message model and schema && made route for viewing all users in chat

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import {connectDB} from "./lib/db.js"
 
 
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
+
 
 app.listen(PORT,()=>{
     console.log("server is running on PORT: "+PORT);
