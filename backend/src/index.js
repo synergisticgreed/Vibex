221478118132323b780f5f60507886c5a1131ba4
synergisticgreed <sameergptnsr@gmail.com>
2025-02-06 12:19:23 +0530

    MONGODB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js"
 
+
+dotenv.config()
 const app=express();
 
 
+const PORT=process.env.PORT;
+
 app.use("/api/auth",authRoutes);
 
-app.listen(5001,()=>{
-    console.log("server is running on port 5001");
+app.listen(PORT,()=>{
+    console.log("server is running on PORT: "+PORT);
+    connectDB()
 })
