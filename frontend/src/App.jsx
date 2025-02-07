f5603fa0c74fa20e30657595717ac5cd1bbbf910
synergisticgreed <sameergptnsr@gmail.com>
2025-02-07 20:10:59 +0530

    Added spinning wheel feature and navigation logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Routes , Route} from 'react-router-dom'
+import { Routes , Route, Navigate} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import SetingsPage from './pages/SetingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
 
 function App() {
+  const {authUser, checkAuth,isCheckingAuth} = useAuthStore()
+
+useEffect(()=>{
+  checkAuth()
+},[checkAuth])
+
+  console.log({authUser});
+
+
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen" >
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
+
   return (
     <div>
       <Navbar/>
 
       <Routes>
-        <Route path="/"  element={<HomePage />} />
-        <Route path="/signup"  element={<SignupPage />} />
-        <Route path="/login"  element={<LoginPage />} />
+        <Route path="/"  element={authUser ? <HomePage /> : <Navigate to="/login" /> } />
+        <Route path="/signup"  element={!authUser ?  <SignupPage /> : <Navigate to="/" />} />
+        <Route path="/login"  element={!authUser ?  <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings"  element={<SetingsPage />} />
-        <Route path="/profile"  element={<ProfilePage />} />
+        <Route path="/profile"  element={authUser ?  <ProfilePage /> : <Navigate to="/login" />} />
 
 
 
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,30 @@
+
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+
+
+export const useAuthStore = create((set)=>({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+    checkAuth: async()=>{
+        try {
+            const res=await axiosInstance.get("/auth/check")    //http://localhost:5001/api  is already here
+            set({authUser: res.data});
+        } catch (error) {
+            console.log("Error in checkAuth: ", error);
+            set({authUser: null});
+        }finally{
+            set({isCheckingAuth: false});
+        }
+    }
+}))
+
+
+
+
+
