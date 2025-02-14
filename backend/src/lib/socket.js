c6dba711cfddc272443431f94046845118823080
synergisticgreed <sameergptnsr@gmail.com>
2025-02-15 00:47:53 +0530

    implemented visibility of online users available

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,11 +12,27 @@ const io = new Server(server,{
         origin: ["http://localhost:5173"]
     }
 })
+//used to store online users
+const userSocketMap={};         //{userId : socketId}
+
+
+
+
 
 io.on("connection",(socket)=>{
     console.log("A user connected", socket.id);
+
+    const userId=socket.handshake.query.userId;
+    if(userId) userSocketMap[userId]=socket.id
+
+
+    // io.emit broadcast to every single user that i am connected
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
     socket.on("disconnect",()=>{
         console.log("A user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
         
     })
 
