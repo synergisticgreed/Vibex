bd3df827f5a58e9ad89ecd887d7ef28bbceb321a
synergisticgreed <sameergptnsr@gmail.com>
2025-02-14 20:27:50 +0530

    Socketio initial setup-server created

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,10 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 import {connectDB} from "./lib/db.js"
+import { app , server} from "./lib/socket.js";
 
 
 dotenv.config()
-const app=express();
 
 
 const PORT=process.env.PORT;
@@ -25,7 +25,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("server is running on PORT: "+PORT);
     connectDB()
 })
---
+++ backend/src/lib/socket.js
@@ -0,0 +1,27 @@
+import {Server} from "socket.io";
+import http from "http";
+import express from "express";
+
+
+
+const express = express();
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors: {
+        origin: ["http://localhost:5173"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log("user connected", socket.id);
+    socket.on("disconnect",()=>{
+        console.log("A user disconnected", socket.id);
+        
+    })
+
+})
+
+
+
+export {io,app,server};
